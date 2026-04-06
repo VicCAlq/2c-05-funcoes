685020cd764909120d8fb51a6e4cba76d9b064b0
mateusnave <mateus.antonio@nave.org.br>
2026-04-06 11:00:27 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- ex-01-funcoes.js
+++ ex-01-funcoes.js
@@ -431,10 +431,11 @@ o nome da função sem os parênteses)
 // Escreva o código da solução abaixo:
 function buscarTravaLinguasComPalavra(word) {
     const newTrava = []
+    const wordFind = ` ${word}`
     let ind = 0
 
     while (ind < travaLinguas.length) {
-        if (travaLinguas[ind].includes(word)) {
+        if (travaLinguas[ind].includes(wordFind)) {
             newTrava.push(travaLinguas[ind])
         }
 
@@ -443,6 +444,7 @@ function buscarTravaLinguasComPalavra(word) {
 
     return newTrava
 }
+
 export const resposta08 = buscarTravaLinguasComPalavra
 
 // Lista para as questões 9 a 12
